9abc8b0911866f7506854fb9a578e12dd39d4b5d
skywalker1995 <ty1995526@outlook.com>
2017-08-07 16:46:17 +0800

    dateTransfer

--- DataAnalystics/src/citi/serviceImpl/DateTransferServiceImpl.java
+++ DataAnalystics/src/citi/serviceImpl/DateTransferServiceImpl.java
@@ -1,19 +1,39 @@
 package citi.serviceImpl;
 
 import java.util.List;
+
 import citi.service.DateTransferService;
+
 public class DateTransferServiceImpl implements DateTransferService {
 
 	@Override
-	public String toDateFromString(String date, String time) {
+	public String toDateFromString(String DBdate, String DBtime) {
 		// TODO Auto-generated method stub
-		return null;
+		String date = DBdate.substring(0, 4) + "-" + DBdate.substring(4, 6)
+				+ "-" + DBdate.substring(6);
+		String time = null;
+		if (DBtime.length() == 3) {
+			time = DBtime.substring(0, 1) + ":" + DBtime.substring(1);
+		} else {
+			time = DBtime.substring(0, 2) + ":" + DBtime.substring(2);
+		}
+		String dateAll = date + " " + time;
+		return dateAll;
 	}
 
+	@SuppressWarnings("null")
 	@Override
 	public List<String> toListFromDate(String date) {
 		// TODO Auto-generated method stub
-		return null;
+		String[] dates = date.split(" ");
+		String[] dmy = dates[0].split("-");
+		String[] smh = dates[1].split(":");
+		String DBdate = dmy[0] + dmy[1] + dmy[2];
+		String DBtime = smh[0] + smh[1];
+		List<String> DBTime = null;
+		DBTime.add(DBdate);
+		DBTime.add(DBtime);
+		return DBTime;
 	}
 
 }
